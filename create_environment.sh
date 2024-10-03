2ca8a243daeeb580b4cdaa4b922742257c8c81e4
sonia250 <s.uwase3@alustudent.com>
2024-10-03 07:49:43 -0700

    updates

---
+++ create_environment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+mkdir -p submission_reminder_app
+
+mkdir -p submission_reminder_app/app
+mkdir -p submission_reminder_app/modules
+mkdir -p submission_reminder_app/config
+mkdir -p submission_reminder_app/assets
+
+touch  submission_reminder_app/app/reminder.sh
+touch  submission_reminder_app/modules/function.sh 
+touch  submission_reminder_app/config/config.env
+touch  submission_reminder_app/startup.sh
+
+~
+~                               
+~
+~
+~
